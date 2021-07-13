c66a4c3ccf79594555e8230d786deab9d539f459
Raghav Gupta <raghav.gupta.dev@gmail.com>
2021-07-13 13:54:56 +0530

    validations improvement

--- models/task.py
+++ models/task.py
@@ -14,16 +14,16 @@ class Task(object):
             "taskName": "string",
             "owner": "string"
         }
-        self.create_required_fields = ["task","owner"]
+        self.create_required_fields = ["taskName","owner"]
         self.create_optional_fields = []
 
-        self.update_required_fields = ["name","owner"]
+        self.update_required_fields = ["taskName"]
         self.update_optional_fields = []
 
 
     def create(self, user):
         # Validator will throw error if invalid
-        #self.validator.validate(user, self.fields, self.create_required_fields, self.create_optional_fields)
+        self.validator.validate(user, self.fields, self.create_required_fields, self.create_optional_fields)
         res = self.db.insert(user, self.collection_name)
         return "Inserted Id " + res
 
@@ -33,7 +33,7 @@ class Task(object):
     def find_by_id(self, id):
         return self.db.find_by_id(id, self.collection_name)    
     def update(self, id, user):
-        #self.validator.validate(user, self.fields, self.update_required_fields, self.update_optional_fields)
+        self.validator.validate(user, self.fields, self.update_required_fields, self.update_optional_fields)
         return self.db.update(id, user,self.collection_name)   
     def delete(self, id):
         return self.db.delete(id, self.collection_name)
